ba5a83ee7477230aa5c52cb4746dfbcc9ef0d0ff
Linn Dahlgren <admin@opposite.biz>
2019-07-13 00:06:09 +0200

    Added rewrite hook for image URLs to use https.

--- src/database.js
+++ src/database.js
@@ -9,19 +9,39 @@ db.version(3).stores({
 
 db.shows.hook("creating", function (primKey, obj, trans) {
   if (typeof obj.name === 'string') obj.nameWords = getAllWords(obj.name);
+  if (typeof obj.thumbUrl === 'string' && obj.thumbUrl.startsWith('http:')) {
+    obj.thumbUrl = 'https' + obj.thumbUrl.substring(4);
+  }
+  if (typeof obj.imageUrl === 'string' && obj.thumbUrl.startsWith('http:')) {
+    obj.imageUrl = 'https' + obj.imageUrl.substring(4);
+  }
 });
 
 db.shows.hook("updating", function (mods, primKey, obj, trans) {
+  const newMods = {}
   if (mods.hasOwnProperty("name")) {
-      // "name" property is being updated
-      if (typeof mods.name == 'string')
-          // "name" property was updated to another valid value. Re-index nameWords:
-          return { nameWords: getAllWords(mods.name) };
-      else
-          // "name" property was deleted (typeof mods.name === 'undefined') or changed to an unknown type. Remove indexes:
-          return { nameWords: [] };
+    // "name" property is being updated
+    if (typeof mods.name == 'string')
+      // "name" property was updated to another valid value. Re-index nameWords:
+      newMods.nameWords = getAllWords(mods.name);
+    else
+      // "name" property was deleted (typeof mods.name === 'undefined') or changed to an unknown type. Remove indexes:
+      newMods.nameWords = [];
   }
 
+  if (mods.hasOwnProperty("thumbUrl")) {
+    if (typeof obj.thumbUrl === 'string' && obj.thumbUrl.startsWith('http:')) {
+      newMods.thumbUrl = 'https' + obj.thumbUrl.substring(4);
+    }
+  }
+
+  if (mods.hasOwnProperty("imageUrl")) {
+    if (typeof obj.imageUrl === 'string' && obj.imageUrl.startsWith('http:')) {
+      newMods.imageUrl = 'https' + obj.imageUrl.substring(4);
+    }
+  }
+
+  return newMods;
 });
 
 const Show = db.shows.defineClass ({
